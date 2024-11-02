efa7ea99da09153be49edabf22d3d18fc072e007
soraiyah <mingreen72@gmail.com>
2024-11-02 19:27:06 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+num1 = int(input(print("enter first number")))
+num2 = int(input(print("enter second number")))
+operation = (input(print("enter operation")))
+
+if operation == ("+"):
+    num3 = num1 + num2
+    print(num3)
+           
+
+if operation == ("/"):
+    num3 = num1 * num2
+    print(num3)
+
+if operation == ("-"):
+    num3 = num1 - num2
+    print(num3)
+
+if operation == ("*")
+
